714ee6c992bfd4beeb4e69ab1da010265c11172f
lc <2981228126@qq.com>
2024-09-19 10:02:16 +0800

    change

---
+++ init.lua
@@ -0,0 +1,7 @@
+require("config.options")
+require("config.keymaps")
+require("config.lazy")
+
+vim.o.background = "dark"
+-- vim.o.background = "light"
+vim.cmd([[colorscheme gruvbox]])
---
+++ lua/config/options.lua
@@ -0,0 +1,17 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.wrap = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
